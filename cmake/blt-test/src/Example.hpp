45e3f5cfe41930381eebcff538191c4be4a87fd8
Cyrus Harrison <cyrush@llnl.gov>
2017-03-07 10:43:58 -0800

    enh: enable travis ci, add internal test project

---
+++ cmake/blt-test/src/Example.hpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * This source code cannot be distributed without permission and
+ * further review from Lawrence Livermore National Laboratory.
+ */
+
+#ifndef _EXAMPLE_HPP_
+#define _EXAMPLE_HPP_
+
+//------------------------------------------------------------------------------
+class Example
+{
+  public:
+     Example();
+    ~Example();
+    
+    bool ReturnTrue();
+};
+#endif
