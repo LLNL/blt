a2ac598bc287096e93168afcd57accd17849002a
Cyrus Harrison <cyrush@llnl.gov>
2017-03-07 12:50:59 -0800

    enable basic testing using travis ci  (#1)
    Enables travis-ci testing of blt via a basic project
    
    Tests using blt with gcc 4.9 and both cmake 3.3.1 and 
    cmake 3.8.0-rc2.
    
    Tests blt's unit tests for gtest, gmock, fortran, openmp, and mpi support.
    
    Removes NUM_PROCS from openmp smoke test, b/c it only works 
    for MPI tests (see github issue #2)

---
+++ cmake/blt-test/src/Example.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * This source code cannot be distributed without permission and
+ * further review from Lawrence Livermore National Laboratory.
+ */
+
+#include "Example.hpp"
+
+//------------------------------------------------------------------------------
+Example::Example()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+Example::~Example()
+{
+    // empty
+}
+
+//------------------------------------------------------------------------------
+bool
+Example::ReturnTrue()
+{
+    return true;
+}
+
