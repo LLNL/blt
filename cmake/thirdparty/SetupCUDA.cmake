59509a668e3de780d0b0b3a42c97cca2f02ffa13
David Beckingsale <davidbeckingsale@gmail.com>
2017-06-14 19:00:38 -0700

    Improve flags for better support of xlC and nvcc combinations

--- cmake/thirdparty/SetupCUDA.cmake
+++ cmake/thirdparty/SetupCUDA.cmake
@@ -8,6 +8,14 @@ message(STATUS "CUDA version:      ${CUDA_VERSION_STRING}")
 message(STATUS "CUDA Include Path: ${CUDA_INCLUDE_DIRS}")
 message(STATUS "CUDA Libraries:    ${CUDA_LIBRARIES}")
 
+# don't propagate host flags - too easy to break stuff!
+set (CUDA_PROPAGATE_HOST_FLAGS Off)
+if (CMAKE_CXX_COMPILER)
+  set (CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
+else ()
+  set (CUDA_HOST_COMPILER ${CMAKE_C_COMPILER})
+endif ()
+
 # depend on 'cuda', if you need to use cuda
 # headers, link to cuda libs, and need to run your source
 # through a cuda compiler (nvcc)
