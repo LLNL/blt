1920c03c6d7162f4d8f3b954a22cc817bad67b73
Kenneth Weiss <weiss27@llnl.gov>
2017-07-06 13:38:17 -0700

    Incorporate the test for gtest death tests into the gtest_smoke test.

--- tests/blt_gtest_smoke.cpp
+++ tests/blt_gtest_smoke.cpp
@@ -41,10 +41,32 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 #include "gtest/gtest.h"
+#include <iostream> 
+
 
 //------------------------------------------------------------------------------
-
+// Simple smoke test for gtest 
+//------------------------------------------------------------------------------
 TEST(blt_gtest_smoke,basic_assert_example)
 {
   EXPECT_TRUE( true );
 }
+
+
+//------------------------------------------------------------------------------
+// Tests the gtest death test feature, which are disabled by default.
+//
+// The call 'assert(false)' should exit the program, passing the test. 
+// Note: To enable death tests, configure BLT with ENABLE_GTEST_DEATH_TESTS.
+//------------------------------------------------------------------------------
+TEST(blt_gtest_smoke,death_test)
+{
+#if (GTEST_HAS_DEATH_TEST == 0)
+  std::cout <<"Note: Death tests disabled in this configuration." << std::endl;
+#endif 
+
+  // Invoke death test function whether death tests enabled or disabled.
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  EXPECT_DEATH_IF_SUPPORTED( assert(false), "");
+}
+
