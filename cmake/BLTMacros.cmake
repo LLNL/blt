c5c580416e7aa27636cded4b96ab5b23b62bfa36
Chris White <white238@llnl.gov>
2017-08-30 15:42:50 -0700

    Add back lost documentation changes

--- cmake/BLTMacros.cmake
+++ cmake/BLTMacros.cmake
@@ -148,7 +148,7 @@ endmacro(blt_add_target_link_flags)
 ## blt_register_library( NAME <libname>
 ##                       DEPENDS_ON [dep1 [dep2 ...]]
 ##                       INCLUDES [include1 [include2 ...]]
-##                       USE_SYSTEM_INCLUDES [ON|OFF]
+##                       TREAT_INCLUDES_AS_SYSTEM [ON|OFF]
 ##                       FORTRAN_MODULES [ path1 [ path2 ..]]
 ##                       LIBRARIES [lib1 [lib2 ...]]
 ##                       COMPILE_FLAGS [ flag1 [ flag2 ..]]
@@ -162,8 +162,8 @@ endmacro(blt_add_target_link_flags)
 ## the DEPENDS_ON in your blt_add_executable call and it will add the INCLUDES
 ## and LIBRARIES to that executable.
 ##
-## USE_SYSTEM_INCLUDES marks the utilizes the compiler's system include path 
-## setting for this libraries include directories.  This is useful if the headers
+## TREAT_INCLUDES_AS_SYSTEM informs the compiler to treat this library's include paths
+## as system headers.  Only some compilers support this. This is useful if the headers
 ## generate warnings you want to not have them reported in your build. This defaults
 ## to OFF.
 ##
@@ -173,7 +173,7 @@ endmacro(blt_add_target_link_flags)
 ## Output variables (name = "foo"):
 ##  BLT_FOO_DEPENDS_ON
 ##  BLT_FOO_INCLUDES
-##  BLT_FOO_USE_SYSTEM_INCLUDES
+##  BLT_FOO_TREAT_INCLUDES_AS_SYSTEM
 ##  BLT_FOO_FORTRAN_MODULES
 ##  BLT_FOO_LIBRARIES
 ##  BLT_FOO_COMPILE_FLAGS
