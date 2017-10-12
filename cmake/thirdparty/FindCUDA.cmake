bc633a39a2b2a1f1b05ef9e48be5e30bb8c2d7cc
Peter B. Robinson <robinson96@llnl.gov>
2017-10-12 10:30:15 -0700

    Link CUDA libraries and executables with nvcc.

--- cmake/thirdparty/FindCUDA.cmake
+++ cmake/thirdparty/FindCUDA.cmake
@@ -1725,7 +1725,9 @@ macro(CUDA_ADD_LIBRARY cuda_target)
 
   # Compute the file name of the intermedate link file used for separable
   # compilation.
-  CUDA_COMPUTE_SEPARABLE_COMPILATION_OBJECT_FILE_NAME(link_file ${cuda_target} "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
+  # commented out since we are linkinig with nvcc. Leaving this around in case we need to 
+  # go back to host compiler linking.
+  #CUDA_COMPUTE_SEPARABLE_COMPILATION_OBJECT_FILE_NAME(link_file ${cuda_target} "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
 
   # Add the library.
   add_library(${cuda_target} ${_cmake_options}
@@ -1737,7 +1739,9 @@ macro(CUDA_ADD_LIBRARY cuda_target)
   # Add a link phase for the separable compilation if it has been enabled.  If
   # it has been enabled then the ${cuda_target}_SEPARABLE_COMPILATION_OBJECTS
   # variable will have been defined.
-  CUDA_LINK_SEPARABLE_COMPILATION_OBJECTS("${link_file}" ${cuda_target} "${_options}" "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
+  # commented out since we are linkinig with nvcc. Leaving this around in case we need to 
+  # go back to host compiler linking.
+  #CUDA_LINK_SEPARABLE_COMPILATION_OBJECTS("${link_file}" ${cuda_target} "${_options}" "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
 
   if(CUDA_SEPARABLE_COMPILATION)
     target_link_libraries(${cuda_target} PUBLIC
@@ -1749,12 +1753,12 @@ macro(CUDA_ADD_LIBRARY cuda_target)
         )
   endif()
 
-  # We need to set the linker language based on what the expected generated file
-  # would be. CUDA_C_OR_CXX is computed based on CUDA_HOST_COMPILATION_CPP.
-  set_target_properties(${cuda_target}
-    PROPERTIES
-    LINKER_LANGUAGE ${CUDA_C_OR_CXX}
-    )
+  # We need to link with nvcc. Host linking prevents calling device methods in one library from kernels in another (yes, even with separable
+  # compilation)
+  string (REPLACE ";" " " LD_FLAGS_STR "${CUDA_NVCC_FLAGS}")
+  set (CMAKE_CUDA_CREATE_STATIC_LIBRARY "${CUDA_NVCC_EXECUTABLE} --lib ${LD_FLAGS_STR} <OBJECTS>  -o <TARGET> " CACHE PATH "" )
+  set (CMAKE_CUDA_CREATE_SHARED_LIBRARY "${CUDA_NVCC_EXECUTABLE} --shared ${LD_FLAGS_STR} <OBJECTS>  -o <TARGET> " CACHE PATH "" )
+  set_target_properties(${cuda_target} PROPERTIES LINKER_LANGUAGE "CUDA")
 
 endmacro()
 
@@ -1774,8 +1778,10 @@ macro(CUDA_ADD_EXECUTABLE cuda_target)
   CUDA_WRAP_SRCS( ${cuda_target} OBJ _generated_files ${_sources} OPTIONS ${_options} )
 
   # Compute the file name of the intermedate link file used for separable
-  # compilation.
-  CUDA_COMPUTE_SEPARABLE_COMPILATION_OBJECT_FILE_NAME(link_file ${cuda_target} "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
+  # compilation. 
+  # commented out since we are linkinig with nvcc. Leaving this around in case we need to 
+  # go back to host compiler linking.
+  #CUDA_COMPUTE_SEPARABLE_COMPILATION_OBJECT_FILE_NAME(link_file ${cuda_target} "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
 
   # Add the library.
   add_executable(${cuda_target} ${_cmake_options}
@@ -1787,16 +1793,18 @@ macro(CUDA_ADD_EXECUTABLE cuda_target)
   # Add a link phase for the separable compilation if it has been enabled.  If
   # it has been enabled then the ${cuda_target}_SEPARABLE_COMPILATION_OBJECTS
   # variable will have been defined.
-  CUDA_LINK_SEPARABLE_COMPILATION_OBJECTS("${link_file}" ${cuda_target} "${_options}" "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
+  # commented out since we are linkinig with nvcc. Leaving this around in case we need to 
+  # go back to host compiler linking.
+  #CUDA_LINK_SEPARABLE_COMPILATION_OBJECTS("${link_file}" ${cuda_target} "${_options}" "${${cuda_target}_SEPARABLE_COMPILATION_OBJECTS}")
 
   target_link_libraries(${cuda_target} PUBLIC ${CUDA_LIBRARIES})
 
-  # We need to set the linker language based on what the expected generated file
-  # would be. CUDA_C_OR_CXX is computed based on CUDA_HOST_COMPILATION_CPP.
-  set_target_properties(${cuda_target}
-    PROPERTIES
-    LINKER_LANGUAGE ${CUDA_C_OR_CXX}
-    )
+  # We need to link with nvcc. Host linking prevents calling device methods in one library from kernels in another (yes, even with separable
+  # compilation)
+  string (REPLACE ";" " " LD_FLAGS_STR "${CUDA_NVCC_FLAGS}")
+  set (CMAKE_CUDA_LINK_EXECUTABLE  "${CUDA_NVCC_EXECUTABLE}  <FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> <LINK_LIBRARIES>" CACHE PATH "" )
+  set_target_properties(${cuda_target} PROPERTIES LINKER_LANGUAGE "CUDA")
+  set_target_properties(${cuda_target} PROPERTIES LINK_FLAGS "${LD_FLAGS_STR}")
 
 endmacro()
 
