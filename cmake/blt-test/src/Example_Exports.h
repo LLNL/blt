55f15ad5a2104e4d2a1f9113519e3800c2cfffe0
Cyrus Harrison <cyrush@llnl.gov>
2017-11-29 12:33:44 -0800

    fixes for windows

--- cmake/blt-test/src/Example_Exports.h
+++ cmake/blt-test/src/Example_Exports.h
@@ -47,21 +47,23 @@
 // -- define proper lib exports for various platforms -- 
 //-----------------------------------------------------------------------------
 #if defined(_WIN32)
-#if defined(WIN32_SHARED_LIBS)
-#if defined(EXAMPLE_EXPORTS) || defined(example_EXPORTS)
-#define EXAMPLE_API __declspec(dllexport)
-#else
-#define EXAMPLE_API __declspec(dllimport)
-#endif
-#endif
-#if defined(_MSC_VER)
-// Turn off warning about lack of DLL interface
-#pragma warning(disable:4251)
-// Turn off warning non-dll class is base for dll-interface class.
-#pragma warning(disable:4275)
-// Turn off warning about identifier truncation
-#pragma warning(disable:4786)
-#endif
+    #if defined(WIN32_SHARED_LIBS)
+        #if defined(EXAMPLE_EXPORTS) || defined(example_EXPORTS)
+            #define EXAMPLE_API __declspec(dllexport)
+        #else
+            #define EXAMPLE_API __declspec(dllimport)
+        #endif
+    #else
+        #define EXAMPLE_API /* not needed for static on windows */
+    #endif
+    #if defined(_MSC_VER)
+        /* Turn off warning about lack of DLL interface */
+        #pragma warning(disable:4251)
+        /* Turn off warning non-dll class is base for dll-interface class. */
+        #pragma warning(disable:4275)
+        /* Turn off warning about identifier truncation */
+        #pragma warning(disable:4786)
+    #endif
 #else
 # if __GNUC__ >= 4 && (defined(EXAMPLE_EXPORTS) || defined(example_EXPORTS))
 #   define EXAMPLE_API __attribute__ ((visibility("default")))
