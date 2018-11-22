7975e8d68378982d561cf59764fd005d0ecd0b94
Kenny Weiss <kenny@kennyweiss.com>
2018-11-21 21:20:40 -0800

    Updates llnl-ray host-config to clang-coral-2018.05.23

--- host-configs/llnl-ray-blue_os-clang-coral@2018.04.17.cmake
+++ host-configs/llnl-ray-blue_os-clang-coral@2018.04.17.cmake
@@ -1,42 +1,49 @@
-##############################################################
+#------------------------------------------------------------------------------
+# Copyright (c) 2018, Lawrence Livermore National Security, LLC.
+#
+# Produced at the Lawrence Livermore National Laboratory
+#
+# LLNL-CODE-725085
+#
+# All rights reserved.
+#
+# This file is part of BLT.
+#
+# For additional details, please also read BLT/LICENSE.
+#------------------------------------------------------------------------------
 # Example host-config file for the blue_os ray cluster at LLNL
-##############################################################
+#------------------------------------------------------------------------------
 #
 # This file provides CMake with paths / details for:
 #  C,C++, MPI and Cuda
 # 
-###########################################################
+#------------------------------------------------------------------------------
 
+set(COMPILER_HOME "/usr/tce/packages/clang/clang-coral-2018.05.23")
 
+# c compiler
+set(CMAKE_C_COMPILER "${COMPILER_HOME}/bin/clang" CACHE PATH "")
+# cpp compiler
+set(CMAKE_CXX_COMPILER "${COMPILER_HOME}/bin/clang++" CACHE PATH "")
 
+set(ENABLE_FORTRAN OFF CACHE BOOL "")
 
-###########################################################
+#------------------------------------------------------------------------------
 # MPI Support
-###########################################################
+#------------------------------------------------------------------------------
 set(ENABLE_MPI ON CACHE BOOL "")
-set(ENABLE_FIND_MPI OFF CACHE BOOL "")
 
-set(MPI_HOME                 "/usr/tce/packages/spectrum-mpi/spectrum-mpi-2017.04.03-clang-coral-2018.04.17" CACHE PATH "")
-set(MPI_C_COMPILER           "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
-set(MPI_CXX_COMPILER         "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
-set(MPI_Fortran_COMPILER     "${MPI_HOME}/bin/mpif90"  CACHE PATH "")
+set(MPI_HOME               "/usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-clang-coral-2018.05.23")
+set(MPI_C_COMPILER         "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
+set(MPI_CXX_COMPILER       "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
 
 set(MPIEXEC                "mpirun"  CACHE PATH "")
 set(MPIEXEC_NUMPROC_FLAG   "-np"     CACHE PATH "")
 set(BLT_MPI_COMMAND_APPEND "mpibind" CACHE PATH "")
 
-###########################################################
-# clang-coral-2018.04.17 compilers
-###########################################################
-# c compiler
-set(CMAKE_C_COMPILER "${MPI_C_COMPILER}" CACHE PATH "")
-# cpp compiler
-set(CMAKE_CXX_COMPILER "${MPI_CXX_COMPILER}" CACHE PATH "")
-
-
-###########################################################
+#------------------------------------------------------------------------------
 # CUDA support
-###########################################################
+#------------------------------------------------------------------------------
 set(ENABLE_CUDA ON CACHE BOOL "")
 
 set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-9.2.64" CACHE PATH "")
@@ -49,5 +56,8 @@ set (CMAKE_CUDA_HOST_COMPILER ${MPI_CXX_COMPILER})
 set (CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" )
 set (CUDA_LINK_WITH_NVCC ON CACHE BOOL "")
 # set the link flags manually since nvcc will link (and not have the wrappers knowledge)
-# on ray - can figure out your equavilant flags by doing mpicc -vvvv
-set (CMAKE_CUDA_LINK_FLAGS "-Xlinker -rpath -Xlinker /usr/tce/packages/spectrum-mpi/ibm/spectrum-mpi-2017.04.03/lib -Xlinker -rpath -Xlinker /usr/tce/packages/clang/clang-coral-2017.10.13/ibm/lib:/usr/tce/packages/gcc/gcc-4.9.3/lib64 -L/usr/tce/packages/spectrum-mpi/ibm/spectrum-mpi-2017.04.03/lib/ -lmpi_ibm" CACHE STRING "")
+# on ray - can figure out your equivalant flags by doing mpicc -vvvv
+set (SPECTRUM_ROLLING "/usr/tce/packages/spectrum-mpi/ibm/spectrum-mpi-rolling-release")
+set (CMAKE_CUDA_LINK_FLAGS "-Xlinker -rpath -Xlinker ${SPECTRUM_ROLLING}/lib -Xlinker -rpath -Xlinker ${COMPILER_HOME}/ibm/lib:/usr/tce/packages/gcc/gcc-4.9.3/lib64 -L${SPECTRUM_ROLLING}/lib/ -lmpi_ibm" CACHE STRING "")
+ 
+ 
