e51e68d4556079789f967bb207fa6a32990d384d
Chris White <white238@llnl.gov>
2019-03-08 16:36:00 -0800

    Fix more inheritance for object libraries

--- tests/internal/src/object_library_test/main.cpp
+++ tests/internal/src/object_library_test/main.cpp
@@ -4,23 +4,46 @@
 // SPDX-License-Identifier: (BSD-3-Clause)
 
 #include "object.hpp"
+#include "inherited_base.hpp"
 
 #include <iostream>
 
 int main()
 {
+    bool failed = false;
+
     int number = object_number();
-    if(number == 6) {
-        std::cout << number
-                  << " was correctly returned from object and base library."
+    if(number != 6) {
+        std::cerr << "Error:"
+                  << number
+                  << " was returned from all libraries."
+                  << std::endl
+                  << "6 was the correct number."
                   << std::endl;
-        return 0;
+        failed = true;
+    } else {
+        std::cout << number
+                  << " was correctly returned from all libraries."
+                  << std::endl;
     }
-    std::cerr << "Error:"
-              << number
-              << " was returned from object and base library."
-              << std::endl
-              << "6 was the correct number."
-              << std::endl;
-    return 1;
+
+    int iNumber = inherited_number();
+    if(iNumber != 3) {
+        std::cerr << "Error:"
+                  << iNumber
+                  << " was returned from the inherited library."
+                  << std::endl
+                  << "3 was the correct number."
+                  << std::endl;
+        failed = true;
+    } else {
+        std::cout << iNumber
+                  << " was correctly returned from inherited library."
+                  << std::endl;
+    }
+
+    if (failed){
+        return 1;
+    }
+    return 0;
 }
