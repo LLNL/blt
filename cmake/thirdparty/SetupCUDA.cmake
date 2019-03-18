1e946f043bb958f4b6cfbe64e5bc199a5556ffc6
David Beckingsale <davidbeckingsale@gmail.com>
2019-03-18 15:27:48 -0700

    Remove quotes surrounding list

--- cmake/thirdparty/SetupCUDA.cmake
+++ cmake/thirdparty/SetupCUDA.cmake
@@ -107,7 +107,7 @@ endif()
 
 set(_cuda_compile_flags " ")
 if (ENABLE_CLANG_CUDA)
-    set (_cuda_compile_flags "-x;cuda;--cuda-gpu-arch=${BLT_CLANG_CUDA_ARCH};--cuda-path=${CUDA_TOOLKIT_ROOT_DIR}")
+    set (_cuda_compile_flags -x cuda --cuda-gpu-arch=${BLT_CLANG_CUDA_ARCH} --cuda-path=${CUDA_TOOLKIT_ROOT_DIR})
     message(STATUS "Clang CUDA Enabled. CUDA compile flags added: ${_cuda_compile_flags}")    
 endif()
 
