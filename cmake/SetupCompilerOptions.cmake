cd4cc9a2e2a7f57d561228fd94cb801a498a93ce
Chris White <white238@llnl.gov>
2019-04-24 17:19:44 -0700

    Add support for C++17

--- cmake/SetupCompilerOptions.cmake
+++ cmake/SetupCompilerOptions.cmake
@@ -226,20 +226,20 @@ if(BUILD_SHARED_LIBS)
 endif()
 
 ################################
-# Enable C++11/14
+# C++ Standard
 ################################
 
 SET( CMAKE_CXX_EXTENSIONS OFF )
 SET( CMAKE_CXX_STANDARD_REQUIRED ON )
+
+set(BLT_CXX_STD "" CACHE STRING "Version of C++ standard")
+set_property(CACHE BLT_CXX_STD PROPERTY STRINGS c++98 c++11 c++17 c++17)
 
 if (BLT_CXX_STD)
     if( BLT_CXX_STD STREQUAL c++98 ) 
         set(CMAKE_CXX_STANDARD 98)
     elseif( BLT_CXX_STD STREQUAL c++11 )
         set(CMAKE_CXX_STANDARD 11)
-        if (ENABLE_CUDA)
-           set(CMAKE_CUDA_STANDARD 11)
-        endif()
         blt_append_custom_compiler_flag(
             FLAGS_VAR CMAKE_CXX_FLAGS
             DEFAULT " "
@@ -247,17 +247,32 @@ if (BLT_CXX_STD)
             PGI "--c++11")
     elseif( BLT_CXX_STD STREQUAL c++14)
         set(CMAKE_CXX_STANDARD 14)
-        if (ENABLE_CUDA)
-           set(CMAKE_CUDA_STANDARD 14)
-        endif()
         blt_append_custom_compiler_flag(
             FLAGS_VAR CMAKE_CXX_FLAGS
             DEFAULT " "
             XL "-std=c++1y"
             PGI "--c++14")
+    elseif( BLT_CXX_STD STREQUAL c++17)
+        # Error out on what does not support C++17
+        if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "XL")
+            message(FATAL_ERROR "XL does not support C++17.")
+        endif()
+        if (ENABLE_CUDA)
+            message(FATAL_ERROR "CMake's CUDA_STANDARD does not support C++17.")
+        endif()
+
+        set(CMAKE_CXX_STANDARD 17)
+        blt_append_custom_compiler_flag(
+            FLAGS_VAR CMAKE_CXX_FLAGS
+            DEFAULT " "
+            PGI "--c++17")
     else()
-        message(FATAL_ERROR "${BLT_CXX_STD} is an invalid entry for BLT_CXX_STD.
-        Valid Options are ( c++98, c++11, c++14 )")
+        message(FATAL_ERROR "${BLT_CXX_STD} is an invalid entry for BLT_CXX_STD. "
+                            "Valid Options are ( c++98, c++11, c++14, c++17 )")
+    endif()
+
+    if (ENABLE_CUDA)
+       set(CMAKE_CUDA_STANDARD ${CMAKE_CXX_STANDARD})
     endif()
 
     message(STATUS "Standard C++${CMAKE_CXX_STANDARD} selected") 
