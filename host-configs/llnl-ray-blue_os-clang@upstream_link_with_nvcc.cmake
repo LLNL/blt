108bfa79cd231eddbf0ce8e8baa71c18768b27a2
Kenneth Weiss <weiss27@llnl.gov>
2019-07-15 16:43:44 -0700

    Updates, renames  and improves llnl's blueos host-config that links with nvcc

--- host-configs/llnl-ray-blue_os-clang@upstream_link_with_nvcc.cmake
+++ host-configs/llnl-ray-blue_os-clang@upstream_link_with_nvcc.cmake
@@ -17,8 +17,7 @@
 #------------------------------------------------------------------------------
 set(ENABLE_FORTRAN OFF CACHE BOOL "")
 
-set(CLANG_VERSION "clang-coral-2018.08.08" CACHE STRING "")
-
+set(CLANG_VERSION "clang-upstream-2019.03.26" CACHE STRING "")
 set(COMPILER_HOME "/usr/tce/packages/clang/${CLANG_VERSION}")
 
 set(CMAKE_C_COMPILER "${COMPILER_HOME}/bin/clang" CACHE PATH "")
@@ -35,10 +34,10 @@ set(MPI_HOME               "/usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-
 set(MPI_C_COMPILER         "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
 set(MPI_CXX_COMPILER       "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
 
-set(MPIEXEC                "mpirun"  CACHE PATH "")
+set(MPIEXEC                "${MPI_HOME}/bin/mpirun"  CACHE PATH "")
 set(MPIEXEC_NUMPROC_FLAG   "-np"     CACHE PATH "")
 set(BLT_MPI_COMMAND_APPEND "mpibind" CACHE PATH "")
-
+set(BLT_MPI_LINK_FLAGS     "-Xlinker -rpath -Xlinker ${MPI_HOME}/lib" CACHE STRING "")
 
 #------------------------------------------------------------------------------
 # OpenMP support
@@ -46,7 +45,8 @@ set(BLT_MPI_COMMAND_APPEND "mpibind" CACHE PATH "")
 set(ENABLE_OPENMP ON CACHE BOOL "")
 
 # Override default link flags because linking with nvcc
-set(BLT_OPENMP_LINK_FLAGS "-Xlinker -rpath -Xlinker /usr/tce/packages/clang/${CLANG_VERSION}/ibm/omprtl/lib -L/usr/tce/packages/clang/${CLANG_VERSION}/ibm/omprtl/lib -lomp -lomptarget-nvptx" CACHE STRING "")
+set(OMP_HOME ${COMPILER_HOME}/ibm/omprtl)
+set(BLT_OPENMP_LINK_FLAGS "-Xlinker -rpath -Xlinker ${OMP_HOME}/lib -L${OMP_HOME}/lib -lomp -lomptarget-nvptx" CACHE STRING "")
 
 
 #------------------------------------------------------------------------------
@@ -54,19 +54,19 @@ set(BLT_OPENMP_LINK_FLAGS "-Xlinker -rpath -Xlinker /usr/tce/packages/clang/${CL
 #------------------------------------------------------------------------------
 set(ENABLE_CUDA ON CACHE BOOL "")
 
-set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-9.2.88" CACHE PATH "")
+set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-9.2.148" CACHE PATH "")
 set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc" CACHE PATH "")
 set(CMAKE_CUDA_HOST_COMPILER ${MPI_CXX_COMPILER} CACHE PATH "")
 
-set (CUDA_ARCH "sm_60" CACHE PATH "")
-set (CMAKE_CUDA_FLAGS "-restrict -arch ${CUDA_ARCH} -std=c++11 --expt-extended-lambda -G" CACHE STRING "" )
+set (_cuda_arch "sm_60")
+set (CMAKE_CUDA_FLAGS "-restrict -arch ${_cuda_arch} -std=c++11 --expt-extended-lambda -G" CACHE STRING "" )
 
 set (CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" )
 set (CUDA_LINK_WITH_NVCC ON CACHE BOOL "")
 # set the link flags manually since nvcc will link (and not have the wrappers knowledge)
 # on ray - can figure out your equivalant flags by doing mpicc -vvvv
-set (SPECTRUM_ROLLING "/usr/tce/packages/spectrum-mpi/ibm/spectrum-mpi-rolling-release")
-set (CMAKE_CUDA_LINK_FLAGS "-Xlinker -rpath -Xlinker ${SPECTRUM_ROLLING}/lib -Xlinker -rpath -Xlinker ${COMPILER_HOME}/ibm/lib:/usr/tce/packages/gcc/gcc-4.9.3/lib64 -L${SPECTRUM_ROLLING}/lib/ -lmpi_ibm" CACHE STRING "")
+set (CMAKE_CUDA_LINK_FLAGS "-Xlinker -rpath -Xlinker ${MPI_HOME}/lib -Xlinker -rpath -Xlinker ${COMPILER_HOME}/ibm/lib:/usr/tce/packages/gcc/gcc-4.9.3/lib64 -L${MPI_HOME}/lib/ -lmpi_ibm" CACHE STRING "")
+
 
 # nvcc does not like gtest's 'pthreads' flag
 set(gtest_disable_pthreads ON CACHE BOOL "")
