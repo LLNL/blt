9e2e74e06862307f63b024305fb72492a7d5c1cc
Chris White <white238@llnl.gov>
2019-08-13 16:24:36 -0700

    Move code health docs

--- cmake/SetupCodeChecks.cmake
+++ cmake/SetupCodeChecks.cmake
@@ -67,23 +67,6 @@ endforeach()
 ## This macro adds all enabled code check targets for the given SOURCES. It
 ## filters based on file extensions.
 ##
-## PREFIX is used in the creation of all the underlying targets. For example:
-## <PREFIX>_uncrustify_check.
-##
-## Sources are filtered based on file extensions for use in these code checks.  If you need
-## additional file extensions defined add them to BLT_C_FILE_EXTS and BLT_Fortran_FILE_EXTS.
-##
-## This macro supports formatting with either Uncrustify or AStyle (but not both) using
-## the following parameters:
-##
-## * UNCRUSTIFY_CFG_FILE is the configuration file for Uncrustify. If UNCRUSTIFY_EXECUTABLE
-##   is defined, found, and UNCRUSTIFY_CFG_FILE is provided it will create both check and
-##   style function for the given C/C++ files.
-##
-## * ASTYLE_CFG_FILE is the configuration file for AStyle. If ASTYLE_EXECUTABLE
-##   is defined, found, and ASTYLE_CFG_FILE is provided it will create both check and
-##   style function for the given C/C++ files.
-##
 ##------------------------------------------------------------------------------
 
 macro(blt_add_code_checks)
@@ -192,29 +175,15 @@ macro(blt_add_code_checks)
 endmacro(blt_add_code_checks)
 
 ##-----------------------------------------------------------------------------
-## blt_add_clang_query_target( NAME             <Created Target Name>
-##                          WORKING_DIRECTORY   <Working Directory>
-##                          COMMENT             <Additional Comment for Target Invocation>
-##                          CHECKERS            <If specified, requires a specific set of checkers>
-##                          DIE_ON_MATCH        <If true, matches stop the build>
-##                          SRC_FILES           [FILE1 [FILE2 ...]] )
+## blt_add_clang_query_target( NAME              <Created Target Name>
+##                             WORKING_DIRECTORY <Working Directory>
+##                             COMMENT           <Additional Comment for Target Invocation>
+##                             CHECKERS          <If specified, requires a specific set of checkers>
+##                             DIE_ON_MATCH      <If true, matches stop the build>
+##                             SRC_FILES         [FILE1 [FILE2 ...]] )
 ##
 ## Creates a new target with the given NAME for running clang_query over the given SRC_FILES
 ##
-## COMMENT is prepended to the commented outputted by CMake.
-##
-## WORKING_DIRECTORY is the directory that clang_query will be ran.  It defaults to the directory
-## where this macro is called.
-##
-## DIE_ON_MATCH This will make a match cause the build to fail, useful if you're using this in
-##              CI to enforce rules about your code
-##
-## CHECKERS are the static analysis passes to specifically run on the target. Options
-##              (no value)          : run all available static analysis checks found
-##              (checker1:checker2) : run checker1 and checker2
-##              (interpreter)       : run the clang-query interpeter to interactively develop queries
-##
-## SRC_FILES is a list of source files that clang_query will be run on.
 ##-----------------------------------------------------------------------------
 macro(blt_add_clang_query_target)
     if(CLANGQUERY_FOUND)
@@ -291,16 +260,6 @@ endmacro(blt_add_clang_query_target)
 ##
 ## Creates a new target with the given NAME for running cppcheck over the given SRC_FILES
 ##
-## PREPEND_FLAGS are additional flags added to the front of the cppcheck flags.
-##
-## APPEND_FLAGS are additional flags added to the end of the cppcheck flags.
-##
-## COMMENT is prepended to the commented outputted by CMake.
-##
-## WORKING_DIRECTORY is the directory that cppcheck will be ran.  It defaults to the directory
-## where this macro is called.
-##
-## SRC_FILES is a list of source files that cppcheck will be run on.
 ##-----------------------------------------------------------------------------
 macro(blt_add_cppcheck_target)
 
@@ -352,24 +311,6 @@ endmacro(blt_add_cppcheck_target)
 ##                            SRC_FILES         [FILE1 [FILE2 ...]] )
 ##
 ## Creates a new target with the given NAME for running uncrustify over the given SRC_FILES.
-##
-## MODIFY_FILES, if set to TRUE, modifies the files in place and adds the created target to
-## the style target.  Otherwise the files are not modified and the created target is added
-## to the check target.
-## Note: Setting MODIFY_FILES to FALSE is only supported in Uncrustify v0.61 or greater.
-##
-## CFG_FILE defines the uncrustify settings.
-##
-## PREPEND_FLAGS are additional flags added to the front of the uncrustify flags.
-##
-## APPEND_FLAGS are additional flags added to the end of the uncrustify flags.
-##
-## COMMENT is prepended to CMake's output for this target.
-##
-## WORKING_DIRECTORY is the directory in which uncrustify will be run.  It defaults 
-## to the directory where this macro is called.
-##
-## SRC_FILES is a list of source files to style/check with uncrustify.
 ##
 ##------------------------------------------------------------------------------
 macro(blt_add_uncrustify_target)
@@ -461,24 +402,6 @@ endmacro(blt_add_uncrustify_target)
 ##                        SRC_FILES         [FILE1 [FILE2 ...]] )
 ##
 ## Creates a new target with the given NAME for running astyle over the given SRC_FILES.
-##
-## MODIFY_FILES, if set to TRUE, modifies the files in place and adds the created target to
-## the style target. Otherwise the files are not modified and the created target is added
-## to the check target.  
-## Note: Setting MODIFY_FILES to FALSE is only supported in AStyle v2.05 or greater.
-##
-## CFG_FILE defines the astyle settings.
-##
-## PREPEND_FLAGS are additional flags added to the front of the astyle flags.
-##
-## APPEND_FLAGS are additional flags added to the end of the astyle flags.
-##
-## COMMENT is prepended to CMake's output for this target.
-##
-## WORKING_DIRECTORY is the directory in which astyle will be run. It defaults to 
-## the directory where this macro is called.
-##
-## SRC_FILES is a list of source files to style/check with astyle.
 ##
 ##------------------------------------------------------------------------------
 macro(blt_add_astyle_target)
