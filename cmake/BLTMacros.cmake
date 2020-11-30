0189dda2cc2703a54377372e6e2a6d0c65413355
Josh Essman <essman1@llnl.gov>
2020-11-30 08:29:53 -0800

    feat: add EXPORTABLE option

--- cmake/BLTMacros.cmake
+++ cmake/BLTMacros.cmake
@@ -528,12 +528,13 @@ endmacro(blt_patch_target)
 ##                     COMPILE_FLAGS [ flag1 [ flag2 ..]]
 ##                     LINK_FLAGS [ flag1 [ flag2 ..]]
 ##                     DEFINES [def1 [def2 ...]] 
-##                     GLOBAL [ON|OFF])
+##                     GLOBAL [ON|OFF]
+##                     EXPORTABLE [ON|OFF])
 ##
 ## Imports a library as a CMake target
 ##------------------------------------------------------------------------------
 macro(blt_import_library)
-    set(singleValueArgs NAME TREAT_INCLUDES_AS_SYSTEM GLOBAL)
+    set(singleValueArgs NAME TREAT_INCLUDES_AS_SYSTEM GLOBAL EXPORTABLE)
     set(multiValueArgs LIBRARIES
                        INCLUDES 
                        DEPENDS_ON
@@ -551,8 +552,12 @@ macro(blt_import_library)
         message(FATAL_ERROR "blt_import_library() must be called with argument NAME <name>")
     endif()
 
-    # Add all imported targets to a single imported interface target
-    if(${arg_GLOBAL})
+    if(${arg_EXPORTABLE})
+        if(${arg_GLOBAL})
+            message(FATAL_ERROR "blt_import_library: EXPORTABLE targets cannot be GLOBAL")
+        endif()
+        add_library(${arg_NAME} INTERFACE)
+    elseif(${arg_GLOBAL})
         add_library(${arg_NAME} INTERFACE IMPORTED GLOBAL)
     else()
         add_library(${arg_NAME} INTERFACE IMPORTED)
