b714bbead60158c233e0c40d9cb0ef2a96e12a5b
Chris White <white238@llnl.gov>
2021-02-04 11:17:04 -0800

    Fix path

--- .gitlab/build_and_test.sh
+++ .gitlab/build_and_test.sh
@@ -13,7 +13,7 @@ rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
-cmake -C host-configs/llnl/$SYS_TYPE/$HOST_CONFIG ../tests/internal
+cmake -C ../host-configs/llnl/$SYS_TYPE/$HOST_CONFIG ../tests/internal
 make -j8
 ctest -DCTEST_OUTPUT_ON_FAILURE=1 --no-compress-output -T Test -VV -j8
 xsltproc -o junit.xml ../tests/ctest-to-junit.xsl Testing/*/Test.xml"
