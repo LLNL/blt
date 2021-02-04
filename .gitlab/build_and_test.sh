d5fd640e72ed9c8a130b079edb15a11e10729919
Chris White <white238@llnl.gov>
2021-02-04 11:16:38 -0800

    Add diagnostic echos

--- .gitlab/build_and_test.sh
+++ .gitlab/build_and_test.sh
@@ -2,11 +2,23 @@
 
 set -e
 
-rm -rf build
-mkdir build
-cd build
+BUILD_DIR=`pwd`/build
+
+echo "~~~~~~~~~~ START:build_and_test.sh ~~~~~~~~~~~"
+echo "CWD="`pwd`
+echo "BUILD_DIR="$BUILD_DIR
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
 
 cmake -C host-configs/llnl/$SYS_TYPE/$HOST_CONFIG ../tests/internal
 make -j8
 ctest -DCTEST_OUTPUT_ON_FAILURE=1 --no-compress-output -T Test -VV -j8
 xsltproc -o junit.xml ../tests/ctest-to-junit.xsl Testing/*/Test.xml"
+
+echo "~~~~~~~~~~ END:build_and_test.sh ~~~~~~~~~~~~~"
+echo "CWD="`pwd`
+echo "BUILD_DIR="$BUILD_DIR
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
