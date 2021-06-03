0cf53c97bec9e1e433af2046bce35ea4477f46c1
Steve Abbott <stephen.abbott@hpe.com>
2021-06-03 12:58:30 -0500

    Update SetupHIP.cmake to use blt_import_library
    The previous implementation added defines to compile flags and explicit
    link flags. Adding defines to DEFINES and libraries to DEPENDS will
    hopefully be more robust.

--- cmake/thirdparty/SetupHIP.cmake
+++ cmake/thirdparty/SetupHIP.cmake
@@ -29,46 +29,52 @@ elseif(${HIP_PLATFORM} STREQUAL "nvcc")
     set(HIP_RUNTIME_INCLUDE_DIRS "${HIP_RUNTIME_INCLUDE_DIRS};${CUDAToolkit_INCLUDE_DIR}")
 endif()
 if ( IS_DIRECTORY "${HIP_ROOT_DIR}/hcc/include" ) # this path only exists on older rocm installs
-        set(HIP_RUNTIME_INCLUDE_DIRS "${HIP_ROOT_DIR}/include;${HIP_ROOT_DIR}/hcc/include" CACHE STRING "")
+    set(HIP_RUNTIME_INCLUDE_DIRS "${HIP_ROOT_DIR}/include;${HIP_ROOT_DIR}/hcc/include" CACHE STRING "")
 else()
-        set(HIP_RUNTIME_INCLUDE_DIRS "${HIP_ROOT_DIR}/include" CACHE STRING "")
+    set(HIP_RUNTIME_INCLUDE_DIRS "${HIP_ROOT_DIR}/include" CACHE STRING "")
 endif()
 set(HIP_RUNTIME_COMPILE_FLAGS "${HIP_RUNTIME_COMPILE_FLAGS};-Wno-unused-parameter")
 
 set(_hip_compile_flags " ")
 if (ENABLE_CLANG_HIP)
-    set(_hip_compile_flags -x hip)
+    if (NOT (${HIP_PLATFORM} STREQUAL "clang"))
+        message(FATAL_ERROR "ENABLE_CLANG_HIP requires HIP_PLATFORM=clang")
+    endif()
+    set(_hip_compile_flags -x;hip)
     # Using clang HIP, we need to construct a few CPP defines and compiler flags
     foreach(arch ${BLT_CLANG_HIP_ARCH})
         string(TOUPPER ${arch} UPARCH)
         string(TOLOWER ${arch} lowarch)
-        list(APPEND _hip_compile_flags "--offload-arch=${lowarch}" "-D__HIP_ARCH_${UPARCH}__=1")
+        list(APPEND _hip_compile_flags "--offload-arch=${lowarch}")
+        set(_hip_compile_defines "${HIP_RUNTIME_DEFINES};-D__HIP_ARCH_${UPARCH}__=1")
     endforeach(arch)
     
     # We need to pass rocm path as well, for certain bitcode libraries.
     # First see if we were given it, then see if it exists in the environment.
     # If not, don't try to guess but print a warning and hope the compiler knows where it is.
-    if(NOT ROCM_PATH)
-        if(DEFINED ENV{ROCM_PATH})
-            set(ROCM_PATH "$ENV{ROCM_PATH}")
-        endif()
+    if (NOT ROCM_PATH)
+        find_path(ROCM_PATH
+            bin/rocminfo
+            ENV ROCM_DIR
+            ENV ROCM_PATH
+            ${HIP_ROOT_DIR}/../
+            ${ROCM_ROOT_DIR}
+            /opt/rocm)
     endif()
 
     if(DEFINED ROCM_PATH)
         list(APPEND _hip_compile_flags "--rocm-path=${ROCM_PATH}")
     else()
-        message(WARNING "ROCM_PATH not found. Set this if the compiler can't find device bitcode libraries.")
+        message(WARN "ROCM_PATH not set or found! This is typically required for Clang HIP Compilation")
     endif()
 
-    message(STATUS "Clang HIP Enabled. HIP compile flags added: ${_hip_compile_flags}")
+    message(STATUS "Clang HIP Enabled. Clang flags for HIP compilation: ${_hip_compile_flags}")
+    message(STATUS "Defines for HIP compilation: ${_hip_compile_defines}")
 
-    # Fundamendally this is just the runtime includes with some extra compile flags
-    blt_register_library(NAME      hip
-                         COMPILE_FLAGS ${_hip_compile_flags}
-                         INCLUDES  ${HIP_RUNTIME_INCLUDE_DIRS}
-                         LIBRARIES ${HIP_RUNTIME_LIBRARIES}
-                         TREAT_INCLUDES_AS_SYSTEM ON)
-
+    blt_import_library(NAME             hip
+                       DEFINES          ${_hip_compile_defines}
+                       COMPILE_FLAGS    ${_hip_compile_flags}
+                       DEPENDS_ON       ${HIP_RUNTIME_LIBRARIES})
 else()
 
     # depend on 'hip', if you need to use hip
@@ -88,6 +94,6 @@ blt_import_library(NAME          hip_runtime
                    INCLUDES      ${HIP_RUNTIME_INCLUDE_DIRS}
                    DEFINES       ${HIP_RUNTIME_DEFINES}
                    COMPILE_FLAGS ${HIP_RUNTIME_COMPILE_FLAGS}
-                   LINK_FLAGS -lamdhip64;-L${HIP_ROOT_DIR}/lib
+                   DEPENDS_ON    ${HIP_RUNTIME_LIBRARIES}
                    TREAT_INCLUDES_AS_SYSTEM ON
                    EXPORTABLE    ${BLT_EXPORT_THIRDPARTY})
