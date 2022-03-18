7e72e9e31bc27bd1afb090dcdd215f4fd89f7f12
Chris White <white238@llnl.gov>
2022-03-18 15:36:13 -0700

    use cmake cuda arch flag

--- host-configs/llnl/blueos_3_ppc64le_ib_p9/clang@upstream_link_with_nvcc.cmake
+++ host-configs/llnl/blueos_3_ppc64le_ib_p9/clang@upstream_link_with_nvcc.cmake
@@ -62,11 +62,12 @@ set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-11.1.1" CACHE PATH "")
 set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc" CACHE PATH "")
 set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER} CACHE PATH "")
 
-set (_cuda_arch "sm_70")
-set (CMAKE_CUDA_FLAGS "-restrict -arch ${_cuda_arch} --expt-extended-lambda -G" CACHE STRING "" )
+set(CMAKE_CUDA_ARCHITECTURES "70" CACHE STRING "")
+set(_cuda_arch "sm_${CMAKE_CUDA_ARCHITECTURES}")
+set(CMAKE_CUDA_FLAGS "-restrict -arch ${_cuda_arch} --expt-extended-lambda -G" CACHE STRING "" )
 
-set (CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" )
-set (CUDA_LINK_WITH_NVCC ON CACHE BOOL "")
+set(CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" )
+set(CUDA_LINK_WITH_NVCC ON CACHE BOOL "")
 
 # nvcc does not like gtest's 'pthreads' flag
 set(gtest_disable_pthreads ON CACHE BOOL "")
