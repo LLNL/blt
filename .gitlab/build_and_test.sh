b13de78d7adb398259e7410d9afc06bb36018797
Chris White <white238@llnl.gov>
2022-03-18 12:10:28 -0700

    make output verbose

--- .gitlab/build_and_test.sh
+++ .gitlab/build_and_test.sh
@@ -14,7 +14,7 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 
 cmake -C ../host-configs/llnl/$SYS_TYPE/$HOST_CONFIG ../tests/internal
-make -j8
+make VERBOSE=1 -j8
 ctest -DCTEST_OUTPUT_ON_FAILURE=1 --no-compress-output -T Test -VV
 xsltproc -o junit.xml ../tests/ctest-to-junit.xsl Testing/*/Test.xml
 
