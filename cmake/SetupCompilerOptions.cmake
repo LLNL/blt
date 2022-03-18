eb9f75055ac3e6d67c1003d965f1fd1bbd800afc
Chris White <white238@llnl.gov>
2022-03-18 15:09:47 -0700

    move compiler standards earlier

--- cmake/SetupCompilerOptions.cmake
+++ cmake/SetupCompilerOptions.cmake
@@ -149,8 +149,14 @@ if(ENABLE_FORTRAN AND BLT_FORTRAN_FLAGS)
     message(STATUS "Updated CMAKE_Fortran_FLAGS to \"${CMAKE_Fortran_FLAGS}\"")
 endif()
 
-if (ENABLE_CUDA AND BLT_CUDA_FLAGS)
-    set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${BLT_CUDA_FLAGS}")
+if(ENABLE_CUDA)
+    if(BLT_CUDA_FLAGS)
+        set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} ${BLT_CUDA_FLAGS}")
+        message(STATUS "Updated CMAKE_CUDA_FLAGS to \"${CMAKE_CUDA_FLAGS}\"")
+    endif()
+    if(CMAKE_CUDA_FLAGS)
+      string(REPLACE "-std=c++${CMAKE_CUDA_STANDARD}" " " CMAKE_CUDA_FLAGS ${CMAKE_CUDA_FLAGS})
+    endif()
     message(STATUS "Updated CMAKE_CUDA_FLAGS to \"${CMAKE_CUDA_FLAGS}\"")
 endif()
 
@@ -222,78 +228,6 @@ if(BUILD_SHARED_LIBS)
     list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
     if("${isSystemDir}" STREQUAL "-1")
         set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-    endif()
-endif()
-
-################################
-# C++ Standard
-################################
-if (NOT DEFINED CMAKE_CXX_EXTENSIONS)
-    message(STATUS "Setting CMAKE_CXX_EXTENSIONS to Off")
-    set( CMAKE_CXX_EXTENSIONS OFF )
-endif ()
-SET( CMAKE_CXX_STANDARD_REQUIRED ON )
-
-set(BLT_CXX_STD "" CACHE STRING "Version of C++ standard")
-set_property(CACHE BLT_CXX_STD PROPERTY STRINGS c++98 c++11 c++14 c++17 c++20)
-
-if (BLT_CXX_STD)
-    if( BLT_CXX_STD STREQUAL c++98 ) 
-        set(CMAKE_CXX_STANDARD 98)
-    elseif( BLT_CXX_STD STREQUAL c++11 )
-        set(CMAKE_CXX_STANDARD 11)
-        blt_append_custom_compiler_flag(
-            FLAGS_VAR CMAKE_CXX_FLAGS
-            DEFAULT " "
-            XL "-std=c++11"
-            PGI "--c++11")
-    elseif( BLT_CXX_STD STREQUAL c++14)
-        set(CMAKE_CXX_STANDARD 14)
-        blt_append_custom_compiler_flag(
-            FLAGS_VAR CMAKE_CXX_FLAGS
-            DEFAULT " "
-            XL "-std=c++14"
-            PGI "--c++14")
-    elseif( BLT_CXX_STD STREQUAL c++17)
-        # Error out on what does not support C++17
-        if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "XL")
-            message(FATAL_ERROR "XL does not support C++17.")
-        endif()
-        if (ENABLE_CUDA AND (NOT DEFINED CMAKE_CUDA_COMPILE_FEATURES OR (NOT "cuda_std_17" IN_LIST CMAKE_CUDA_COMPILE_FEATURES)))
-            message(FATAL_ERROR "CMake's CUDA_STANDARD does not support C++17.")
-        endif()
-
-        set(CMAKE_CXX_STANDARD 17)
-        blt_append_custom_compiler_flag(
-            FLAGS_VAR CMAKE_CXX_FLAGS
-            DEFAULT " "
-            PGI "--c++17")
-    elseif( BLT_CXX_STD STREQUAL c++20)
-        # Error out on what does not support C++20
-        if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "XL")
-            message(FATAL_ERROR "XL does not support C++20.")
-        endif()
-        if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "PGI")
-            message(FATAL_ERROR "PGI is not yet supported using C++20.")
-        endif()
-        if (ENABLE_CUDA AND (NOT DEFINED CMAKE_CUDA_COMPILE_FEATURES OR (NOT "cuda_std_20" IN_LIST CMAKE_CUDA_COMPILE_FEATURES)))
-            message(FATAL_ERROR "CMake's CUDA_STANDARD does not support C++20.")
-        endif()
-
-        set(CMAKE_CXX_STANDARD 20)
-    else()
-        message(FATAL_ERROR "${BLT_CXX_STD} is an invalid entry for BLT_CXX_STD. "
-                            "Valid Options are ( c++98, c++11, c++14, c++17, c++20 )")
-    endif()
-
-    message(STATUS "Standard C++${CMAKE_CXX_STANDARD} selected")
-
-    if (ENABLE_CUDA)
-       if (NOT DEFINED CMAKE_CUDA_STANDARD)
-          set(CMAKE_CUDA_STANDARD ${CMAKE_CXX_STANDARD})
-       endif()
-
-       message(STATUS "Standard C++${CMAKE_CUDA_STANDARD} selected for CUDA")
     endif()
 endif()
 
