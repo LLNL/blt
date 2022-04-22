5a36306a6306455395ede69eab4c5f18d1b19008
Alan Dayton <dayton8@llnl.gov>
2022-04-22 13:16:59 -0700

    Change order of operations

--- cmake/BLTMacros.cmake
+++ cmake/BLTMacros.cmake
@@ -1421,7 +1421,7 @@ macro(blt_convert_to_system_includes)
     endif()
 
     get_target_property(_include_dirs ${arg_TARGET} INTERFACE_INCLUDE_DIRECTORIES)
-    set_property(TARGET ${arg_TARGET} APPEND PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_include_dirs}")
     set_property(TARGET ${arg_TARGET} PROPERTY INTERFACE_INCLUDE_DIRECTORIES "")
+    set_property(TARGET ${arg_TARGET} APPEND PROPERTY INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_include_dirs}")
 endmacro()
 
