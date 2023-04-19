c0a881c568613be9cf91bcf59789f13c4e3b1c37
Alex Tyler Chapman <chapman39@llnl.gov>
2023-04-19 16:19:34 -0700

    Revert "remove no tail call and add patch"
    This reverts commit db22bebf13974686fb7ce2a5bf861ba8cd13d819.

--- thirdparty_builtin/googletest/googletest/src/gtest-internal-inl.h
+++ thirdparty_builtin/googletest/googletest/src/gtest-internal-inl.h
@@ -636,9 +636,8 @@ class GTEST_API_ UnitTestImpl {
   // For example, if Foo() calls Bar(), which in turn calls
   // CurrentOsStackTraceExceptTop(1), Foo() will be included in the
   // trace but Bar() and CurrentOsStackTraceExceptTop() won't.
-  // BLT UPDATE: remove GTEST_NO_TAIL_CALL_
   std::string CurrentOsStackTraceExceptTop(int skip_count)
-      GTEST_NO_INLINE_;
+      GTEST_NO_INLINE_ GTEST_NO_TAIL_CALL_;
 
   // Finds and returns a TestSuite with the given name.  If one doesn't
   // exist, creates one and returns it.
