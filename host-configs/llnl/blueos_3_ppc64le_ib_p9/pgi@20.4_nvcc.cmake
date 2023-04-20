ac10375fce519d374e1f91e1cbb976eb7b134860
Alex Tyler Chapman <chapman39@llnl.gov>
2023-04-20 12:36:18 -0700

    update hc

--- host-configs/llnl/blueos_3_ppc64le_ib_p9/pgi@20.4_nvcc.cmake
+++ host-configs/llnl/blueos_3_ppc64le_ib_p9/pgi@20.4_nvcc.cmake
@@ -6,20 +6,37 @@
 #------------------------------------------------------------------------------
 # Example host-config file for using PGI and CUDA
 #------------------------------------------------------------------------------
-set (CMAKE_CXX_COMPILER "/usr/tce/packages/pgi/pgi-20.4/bin/pgc++" CACHE PATH "")
-set (CMAKE_C_COMPILER "/usr/tce/packages/pgi/pgi-20.4/bin/pgcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/pgi/pgi-20.4/bin/pgc++" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/tce/packages/pgi/pgi-20.4/bin/pgcc" CACHE PATH "")
 
 set(ENABLE_FORTRAN OFF CACHE BOOL "")
-set(ENABLE_MPI OFF CACHE BOOL "")
+
+#------------------------------------------------------------------------------
+# Extra options and flags
+#------------------------------------------------------------------------------
+
 set(ENABLE_OPENMP OFF CACHE BOOL "")
+set(ENABLE_MPI OFF CACHE BOOL "")
+
+
+#------------------------------------------------------------------------------
+# CUDA support
+#------------------------------------------------------------------------------
 
 set(ENABLE_CUDA ON CACHE BOOL "")
 
-set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-11.1.1" CACHE PATH "")
+set(CUDA_TOOLKIT_ROOT_DIR "/usr/tce/packages/cuda/cuda-11.2.0" CACHE PATH "")
 set(CMAKE_CUDA_COMPILER "${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc" CACHE PATH "")
-set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER} CACHE PATH "")
+set(CMAKE_CUDA_HOST_COMPILER "${CMAKE_CXX_COMPILER}" CACHE PATH "")
+
+set(CMAKE_CUDA_ARCHITECTURES "70" CACHE STRING "")
+set(CMAKE_CUDA_FLAGS "-restrict --expt-extended-lambda -G" CACHE STRING "")
 
 set(CUDA_SEPARABLE_COMPILATION ON CACHE BOOL "" )
 
-set (_cuda_arch "sm_70")
-set (CMAKE_CUDA_FLAGS "-restrict -arch ${_cuda_arch} -std=c++11 --expt-extended-lambda -G" CACHE STRING "" )
+# nvcc does not like gtest's 'pthreads' flag
+set(gtest_disable_pthreads ON CACHE BOOL "")
+
+# Very specific fix for working around CMake adding implicit link directories returned by the BlueOS
+# compilers to link CUDA executables 
+set(BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE "/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/lib64" CACHE STRING "")
