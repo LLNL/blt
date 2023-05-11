4713afce2bba59b565ac7bd3b8e862fefb4d9eba
Alex Tyler Chapman <chapman39@llnl.gov>
2023-05-11 10:22:11 -0700

    remove docs hook

--- host-configs/llnl/toss_4_x86_64_ib/gcc@10.3.1.cmake
+++ host-configs/llnl/toss_4_x86_64_ib/gcc@10.3.1.cmake
@@ -34,7 +34,6 @@ set(CMAKE_Fortran_COMPILER "${GCC_HOME}/bin/gfortran" CACHE PATH "")
 #------------------------------------------------------------------------------
 # MPI Support
 #------------------------------------------------------------------------------
-#_blt_tutorial_mpi_config_start
 set(ENABLE_MPI ON CACHE BOOL "")
 
 set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.3.6-${GCC_VERSION}" CACHE PATH "")
@@ -45,4 +44,3 @@ set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpif90" CACHE PATH "")
 
 set(MPIEXEC              "/usr/bin/srun" CACHE PATH "")
 set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
-#_blt_tutorial_mpi_config_end
