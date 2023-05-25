2c7908a7cc7997ac374df9bc30514a472f5052ec
Chris White <white238@llnl.gov>
2023-05-25 14:12:45 -0700

    clean up host-configs and ci jobs

--- host-configs/llnl/toss_4_x86_64_ib/intel@2022.1.0-c++17.cmake
+++ host-configs/llnl/toss_4_x86_64_ib/intel@2022.1.0-c++17.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2023, Lawrence Livermore National Security, LLC and
+# Copyright (c) 2017-2021, Lawrence Livermore National Security, LLC and
 # other BLT Project Developers. See the top-level LICENSE file for details
 # 
 # SPDX-License-Identifier: (BSD-3-Clause)
@@ -13,30 +13,36 @@
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
-# gcc@8.3.1 compilers
+# intel-2022.1.0 compilers
 #------------------------------------------------------------------------------
 
-set(GCC_VERSION "gcc-8.3.1")
-set(GCC_HOME "/usr/tce/packages/gcc/${GCC_VERSION}")
+set(INTELLLVM_VERSION "intel-2022.1.0-magic")
+set(INTELLLVM_HOME "/usr/tce/packages/intel/${INTELLLVM_VERSION}")
 
 # c compiler
-set(CMAKE_C_COMPILER "${GCC_HOME}/bin/gcc" CACHE PATH "")
+set(CMAKE_C_COMPILER "${INTELLLVM_HOME}/bin/icx" CACHE PATH "")
 
 # cpp compiler
-set(CMAKE_CXX_COMPILER "${GCC_HOME}/bin/g++" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "${INTELLLVM_HOME}/bin/icpx" CACHE PATH "")
 
 # fortran support
 set(ENABLE_FORTRAN ON CACHE BOOL "")
 
 # fortran compiler
-set(CMAKE_Fortran_COMPILER "${GCC_HOME}/bin/gfortran" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "${INTELLLVM_HOME}/bin/ifx" CACHE PATH "")
+
+#------------------------------------------------------------------------------
+# Extra flags
+#------------------------------------------------------------------------------
+
+set(BLT_CXX_STD "c++17")
 
 #------------------------------------------------------------------------------
 # MPI Support
 #------------------------------------------------------------------------------
 set(ENABLE_MPI ON CACHE BOOL "")
 
-set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.3-${GCC_VERSION}" CACHE PATH "")
+set(MPI_HOME             "/usr/tce/packages/mvapich2/mvapich2-2.3.6-${INTELLLVM_VERSION}" CACHE PATH "")
 
 set(MPI_C_COMPILER       "${MPI_HOME}/bin/mpicc" CACHE PATH "")
 set(MPI_CXX_COMPILER     "${MPI_HOME}/bin/mpicxx" CACHE PATH "")
@@ -44,8 +50,3 @@ set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpif90" CACHE PATH "")
 
 set(MPIEXEC              "/usr/bin/srun" CACHE PATH "")
 set(MPIEXEC_NUMPROC_FLAG "-n" CACHE PATH "")
-
-#------------------------------------------------------------------------------
-# Enable TPL export
-#------------------------------------------------------------------------------
-set(BLT_EXPORT_THIRDPARTY ON CACHE BOOL "")
