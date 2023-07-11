d0c2f0c0c776b25d15631439e05b2e39b2aa1f3f
john bowen <john.bowen42@gmail.com>
2023-07-11 14:30:35 -0700

    format SetupOpenMP

--- cmake/thirdparty/SetupOpenMP.cmake
+++ cmake/thirdparty/SetupOpenMP.cmake
@@ -53,6 +53,6 @@ message(STATUS "OpenMP Compile Flags: ${_compile_flags}")
 message(STATUS "OpenMP Link Flags:    ${_link_flags}")
 
 blt_import_library(NAME openmp
-                 COMPILE_FLAGS ${_compile_flags}
-                 LINK_FLAGS    ${_link_flags}
-                 EXPORTABLE    ${BLT_EXPORT_THIRDPARTY})
+                   COMPILE_FLAGS ${_compile_flags}
+                   LINK_FLAGS    ${_link_flags}
+                   EXPORTABLE    ${BLT_EXPORT_THIRDPARTY})
