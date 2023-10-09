689fea0a3ed841141bbcdd87fe94fc96b5e17aba
john bowen <john.bowen42@gmail.com>
2023-10-09 09:47:58 -0700

    Add git submodule update command and cd into submodule

--- .gitlab/run_blt_test.sh
+++ .gitlab/run_blt_test.sh
@@ -2,5 +2,9 @@
 
 BLT_DIR=`pwd`
 HOST_CONFIG=`pwd`/blt_test/$HOST_CONFIG
+RUNNER_SCRIPT=`pwd`/blt_test/run_tests.py
+git submodule update --init --recursive
+ls `pwd`/blt_test/
+cd `pwd`/blt_test/
 
-python3 run_tests.py --run-test $ENABLED_BLT_TESTS --host-config $HOST_CONFIG --verbose --clean --blt-source-dir $BLT_DIR
+python3 $RUNNER_SCRIPT --run-test $ENABLED_BLT_TESTS --host-config $HOST_CONFIG --verbose --clean --blt-source-dir $BLT_DIR
