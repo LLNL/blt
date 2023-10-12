fd87a2463463633ad4963c57d84034713dd6d392
john bowen <bowen36@llnl.gov>
2023-10-12 09:46:23 -0700

    Move blt_test pin temporarily and add host config for cpu tests

--- .gitlab/run_blt_test.sh
+++ .gitlab/run_blt_test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 BLT_DIR=`pwd`
-HOST_CONFIG=`pwd`/blt_test/$HOST_CONFIG
 RUNNER_SCRIPT=`pwd`/blt_test/run_tests.py
 git submodule update --init --recursive
-ls `pwd`/blt_test/
 cd `pwd`/blt_test/
 
 if [[ -z $HOST_CONFIG  ]]; then
+    HOST_CONFIG=`pwd`/blt_test/$HOST_CONFIG
     python3 $RUNNER_SCRIPT --run-test $ENABLED_BLT_TESTS --verbose --clean --blt-source-dir $BLT_DIR
 else
     python3 $RUNNER_SCRIPT --run-test $ENABLED_BLT_TESTS --host-config $HOST_CONFIG --verbose --clean --blt-source-dir $BLT_DIR
