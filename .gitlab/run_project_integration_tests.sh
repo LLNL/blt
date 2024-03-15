f6a4f0530b208b7772abf8044c73ea20f58e21eb
Chris White <white238@llnl.gov>
2024-03-15 15:56:00 -0700

    simplify using path

--- .gitlab/run_project_integration_tests.sh
+++ .gitlab/run_project_integration_tests.sh
@@ -2,11 +2,7 @@
 
 BLT_DIR=`pwd`
 RUNNER_SCRIPT=`pwd`/tests/projects/run_tests.py
-cd `pwd`/tests/projects/
+HOST_CONFIG_PATH=`pwd`/host-configs/llnl/$SYS_TYPE/$HOST_CONFIG
 
-if [[ -z $HOST_CONFIG  ]]; then
-    HOST_CONFIG=`pwd`/host-configs/llnl/$SYS_TYPE/$HOST_CONFIG
-    python3 $RUNNER_SCRIPT --run-test $ENABLED_BLT_TESTS --verbose --clean --blt-source-dir $BLT_DIR
-else
-    python3 $RUNNER_SCRIPT --run-test $ENABLED_BLT_TESTS --host-config $HOST_CONFIG --verbose --clean --blt-source-dir $BLT_DIR
-fi
+cd `pwd`/tests/projects/
+python3 $RUNNER_SCRIPT --run-test $ENABLED_BLT_TESTS --host-config $HOST_CONFIG_PATH --verbose --clean --blt-source-dir $BLT_DIR
