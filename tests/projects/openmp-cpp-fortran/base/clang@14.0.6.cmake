01ef6270f4b7a05036f4f6b866bb26eb99eb0852
Chris White <white238@llnl.gov>
2024-03-15 15:46:33 -0700

    use all blessed host-configs and remove old ones"

--- tests/projects/openmp-cpp-fortran/base/clang@14.0.6.cmake
+++
@@ -1,4 +0,0 @@
-set(CLANG_VERSION "clang-14.0.6")
-set(CLANG_HOME "/usr/tce/packages/clang/${CLANG_VERSION}")
-set(CMAKE_C_COMPILER "${CLANG_HOME}/bin/clang" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "${CLANG_HOME}/bin/clang++" CACHE PATH "")
