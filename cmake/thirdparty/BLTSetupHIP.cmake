889af7556f3062a0f4450474ed5cf4f4253af5b6
David Beckingsale <davidbeckingsale@gmail.com>
2024-10-29 15:52:07 -0700

    Fixup downstream usage

--- cmake/thirdparty/BLTSetupHIP.cmake
+++ cmake/thirdparty/BLTSetupHIP.cmake
@@ -93,13 +93,15 @@ else()
 endif()
 
 blt_import_library(NAME          blt_hip
-                   COMPILE_FLAGS ${_blt_hip_compile_flags}
+                   #COMPILE_FLAGS ${_blt_hip_compile_flags}
                    EXPORTABLE    ${BLT_EXPORT_THIRDPARTY}
                    GLOBAL        ${_blt_hip_is_global})
 
 # Hard-copy inheritable properties instead of depending on hip::device so that we can export
 # all required information in our target blt_hip
-blt_inherit_target_info(TO blt_hip FROM hip::device OBJECT FALSE)
+#blt_inherit_target_info(TO blt_hip FROM hip::device OBJECT FALSE)
+# Needed for propogation of necessary libraries/includes
+blt_inherit_target_info(TO blt_hip FROM hip::host OBJECT FALSE)
 
 add_library(blt::hip ALIAS blt_hip)
 
