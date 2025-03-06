6e84ef47e4462d8fe776ef2accbcc9c4ba8a3a26
David Beckingsale <davidbeckingsale@gmail.com>
2025-03-06 08:43:29 -0800

    Fixup linker flags

--- host-configs/llnl/toss_4_x86_64_ib_cray/clang@18.0.0_hip.cmake
+++ host-configs/llnl/toss_4_x86_64_ib_cray/clang@18.0.0_hip.cmake
@@ -59,7 +59,7 @@ set(ENABLE_HIP ON CACHE BOOL "")
 
 set(CMAKE_HIP_ARCHITECTURES "gfx90a" CACHE STRING "")
 
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,--disable-new-dtags -L${ROCM_ROOT_DIR}/hip/../llvm/lib -L${ROCM_ROOT_DIR}/hip/lib -Wl,-rpath,${ROCM_ROOT_DIR}/hip/../llvm/lib:${ROCM_ROOT_DIR}/hip/lib -lpgmath -lflang -lflangrti -lompstub -lamdhip64  -L${ROCM_ROOT_DIR}/hip/../lib64 -Wl,-rpath,${ROCM_ROOT_DIR}/hip/../lib64  -L${ROCM_ROOT_DIR}/hip/../lib -Wl,-rpath,${ROCM_ROOT_DIR}/hip/../lib -lamd_comgr -lhsa-runtime64 " CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--disable-new-dtags -L${ROCM_ROOT_DIR}/llvm/lib -L${ROCM_ROOT_DIR}/lib -Wl,-rpath,${ROCM_ROOT_DIR}/llvm/lib:${ROCM_ROOT_DIR}/lib -lpgmath -lflang -lflangrti -lompstub -lamdhip64  -L${ROCM_ROOT_DIR}/lib64 -Wl,-rpath,${ROCM_ROOT_DIR}/lib64  -L${ROCM_ROOT_DIR}/lib -Wl,-rpath,${ROCM_ROOT_DIR}/lib -lamd_comgr -lhsa-runtime64 " CACHE STRING "")
 
 #_blt_tutorial_useful_hip_variables_end
 
